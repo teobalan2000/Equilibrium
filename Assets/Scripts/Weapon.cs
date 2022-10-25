d282849325c4030dbab25b57c8f07448fe8e0d01
BVS <vladbalan002@yahoo.com>
2022-10-25 03:53:18 +0300

    Stuff
    -Player Collider

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+	public Joystick joystick;
+	public GameObject Object;
+	Vector2 GameobjectRotation;
+	private float GameobjectRotation2;
+	private float GameobjectRotation3;
+	public GameObject FirePoint;
+
+	public bool FacingRight = true;
+
+	void Update()
+	{
+		//Gets the input from the jostick
+		GameobjectRotation = new Vector2(joystick.Horizontal, joystick.Vertical);
+
+		GameobjectRotation3 = GameobjectRotation.x;
+
+		if (FacingRight)
+		{
+			//Rotates the object if the player is facing right
+			GameobjectRotation2 = GameobjectRotation.x + GameobjectRotation.y * 90;
+			Object.transform.rotation = Quaternion.Euler(0f, 0f, GameobjectRotation2);
+		}
+		else
+		{
+			//Rotates the object if the player is facing left
+			GameobjectRotation2 = GameobjectRotation.x + GameobjectRotation.y * -90;
+			Object.transform.rotation = Quaternion.Euler(0f, 180f, -GameobjectRotation2);
+		}
+		if (GameobjectRotation3 < 0 && FacingRight)
+		{
+			// Executes the void: Flip()
+			Flip();
+		}
+		else if (GameobjectRotation3 > 0 && !FacingRight)
+		{
+			// Executes the void: Flip()
+			Flip();
+		}
+	}
+	private void Flip()
+	{
+		// Flips the player.
+		FacingRight = !FacingRight;
+		//Object.transform.Rotate(0, 0, 0);
+		transform.Rotate(-45, 180, 0);
+	}
+}
